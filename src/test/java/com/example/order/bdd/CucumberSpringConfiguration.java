1c2bd18dc7506e56a39876a98da1de317b27602f
craspoo <camilarspoo@gmail.com>
2026-01-05 22:14:34 -0300

    com testes

--- src/test/java/com/example/order/bdd/CucumberSpringConfiguration.java
+++ src/test/java/com/example/order/bdd/CucumberSpringConfiguration.java
@@ -1,6 +1,6 @@
 package com.example.order.bdd;
 
-import com.example.order.webhook.PaymentServicePort; // <--- Confira se o import está certo
+//import com.example.order.webhook.PaymentServicePort; // <--- Confira se o import está certo
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,17 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 @CucumberContextConfiguration
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(CucumberSpringConfiguration.BddTestConfig.class) // <--- Forçamos a importação aqui
+//@Import(CucumberSpringConfiguration.BddTestConfig.class) // <--- Forçamos a importação aqui
 public class CucumberSpringConfiguration {
-
-    // Criamos uma classe interna dedicada para configurar os Mocks
-    @TestConfiguration
-    static class BddTestConfig {
-
-        @Bean
-        public PaymentServicePort paymentServicePort() {
-            // Cria um Mock para enganar o Spring e permitir que a aplicação suba
-            return Mockito.mock(PaymentServicePort.class);
-        }
-    }
 }
