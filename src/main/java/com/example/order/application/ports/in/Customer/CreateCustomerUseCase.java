d92118299014b1048ac6d8aed17f124e149c47f1
2rspoo <rodrigo.spoo@gmail.com>
2026-01-12 21:14:29 -0300

    Ajustando app

--- src/main/java/com/example/order/application/ports/in/Customer/CreateCustomerUseCase.java
+++ src/main/java/com/example/order/application/ports/in/Customer/CreateCustomerUseCase.java
@@ -4,5 +4,6 @@ package com.example.order.application.ports.in.Customer;
 import com.example.order.domain.entities.Customer;
 
 public interface CreateCustomerUseCase {
+
     Customer execute(Customer customer);
 }
